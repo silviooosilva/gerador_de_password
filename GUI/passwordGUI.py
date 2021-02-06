870a374076bcd479c5f6dc090f822e99922d3c77
Nurul-GC <Nurul-GC@github.com>
2021-02-06 04:37:14 +0000

    updating commit

--- GUI/passwordGUI.py
+++ GUI/passwordGUI.py
@@ -9,24 +9,29 @@ class PWD(QApplication):
     def __init__(self):
         super().__init__(argv)
         self.ferramentas = QWidget()
-        self.ferramentas.setWindowIcon(QIcon("img/favicon/favicon-32x32.png"))
+        self.ferramentas.setWindowIcon(QIcon("img/favicon/favicon-16x16.png"))
         self.ferramentas.setWindowTitle("Gerador de Passwords")
         self.ferramentas.setPalette(QPalette(QColor('blue')))
-        self.ferramentas.setFixedSize(500, 500)
+        self.ferramentas.setFixedSize(500, 300)
 
         menu = QMenuBar(self.ferramentas)
+        instr = menu.addAction("Instruções")
+        instr.triggered.connect(self.instrucao)
         sobre = menu.addAction("Sobre")
         sobre.triggered.connect(self.sobre)
 
         self.tab = QTabWidget(self.ferramentas)
-        self.tab.setGeometry(0, 25, 500, 480)
+        self.tab.setGeometry(0, 25, 500, 280)
 
         self.principal()
 
     def principal(self):
         def codificar():
-            password = encryptMessage(None, pwd.text())
-            return labelPwd.setText(password)
+            if pwd.text() == '' or len(pwd.text()) <= 5:
+                QMessageBox.warning(self.ferramentas, 'Atenção', 'Digite antes uma palavra com mais de 6 caracteres\npara que a codificação seja bem sucedida..')
+            else:
+                password = encryptMessage(None, pwd.text())
+                return labelPwd.setText(password)
 
         janela1 = QWidget()
         layout2 = QVBoxLayout()
@@ -35,10 +40,10 @@ class PWD(QApplication):
         labelIntro.setAlignment(Qt.AlignCenter)
         layout2.addWidget(labelIntro)
 
-        labelImagem = QLabel()
-        labelImagem.setPixmap(QPixmap("img/01.png"))
-        labelImagem.setAlignment(Qt.AlignCenter)
-        layout2.addWidget(labelImagem)
+        # labelImagem = QLabel()
+        # labelImagem.setPixmap(QPixmap("img/01.png"))
+        # labelImagem.setAlignment(Qt.AlignCenter)
+        # layout2.addWidget(labelImagem)
 
         labelPwd = QLabel("...")
         labelPwd.setAlignment(Qt.AlignCenter)
@@ -47,6 +52,7 @@ class PWD(QApplication):
 
         layoutPwd = QFormLayout()
         pwd = QLineEdit()
+        pwd.returnPressed.connect(codificar)
         layoutPwd.addRow('<b>Digite a palavra: *</b>', pwd)
         layout2.addLayout(layoutPwd)
 
@@ -74,6 +80,19 @@ Nome: Gerador de Passwords
 Versão: 1.1
 Programador: Silvio Silva
 Designer: Nurul-GC
+""")
+
+    def instrucao(self):
+        QMessageBox.information(self.ferramentas, 'Instruções', """
+COMO CRIAR PASSWORDS SEGURAS: 
+
+[1] - NÃO USE SEU NOME PRÓPRIO OU DE ALGUM FAMILIAR;
+[2] - UTILIZE CARACTERES ESPECIAIS (SE PERMITIDO), COMO O @;
+[3] - FAÇA A JUNÇÃO DE LETRAS MAIÚSCULAS E MINÚSCULAS NAS SUAS PASSWORDS;
+[4] - UTILIZE NÚMEROS;
+[5] - UTILIZE SENHAS MAIORES OU IGUAIS A SEIS(6) CARÁCTERES;
+
+CRIE BOAS PASSWORDS E SALVE SUAS INFORMAÇÕES PESSOAIS!!!
 """)
 
 
--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-# gerador_de_password
-Gere passwords seguras, usando este programinha! Verifique também se a sua password é segura ou não!
+# ![](img/favicon/favicon-32x32.png) - Gerador de Passwords
+
+![](img/01.jpg)
+
+## CLI
+**Menu Principal** \
+![](img/menu_principal.png)
+
+**Menu Ajuda CLI** \
+![](img/menu_ajuda.png)
+
+---
+
+## GUI
+**Janela Principal** \
+![](img/janela_principal.png)
+
+**Codificado** \
+![](img/codificado.png)
+`* a codificação é feita de forma dinámica, o que significa
+que uma palavra pode ter varias cifras e também é valida a
+codificação de palavras separadas ex: 'meu amigo'..`
+
+**Copiar para Colar** \
+![](img/copy_paste.png)
+`* a opção de copiar serve para transferir o texto codificado
+para o clipboard do sistema e facilitar para que o usuario
+apenas cole o texto onde for preciso..`
+
+**Retornando Erro** \
+![](img/erro.png)
+`* a palavra introduzida deve ter no minimo 6 caracteres..`
+
+**Sobre o Programa** \
+![](img/sobre.png)
+
+---
+
+**_Gere passwords seguras, usando este programinha! Verifique também se a sua password é segura ou não!
 Agradeço todas as opiniões e críticas! 
-Sou Beginner na linguagem 🖖😅
+Sou Beginner na linguagem_** - [Silvio Silva](/#) 🖖😅
